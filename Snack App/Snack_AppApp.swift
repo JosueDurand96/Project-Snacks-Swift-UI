b43de5dea26952e879d617b526bedca24a455680
Josué Durand Pebe <josue.durand8@gmail.com>
2025-05-02 12:22:10 -0500

    Initial Commit

---
+++ Snack App/Snack_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Snack_AppApp.swift
+//  Snack App
+//
+//  Created by Josue Durand Pebe on 2/05/25.
+//
+
+import SwiftUI
+
+@main
+struct Snack_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Snack AppTests/Snack_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Snack_AppTests.swift
+//  Snack AppTests
+//
+//  Created by Josue Durand Pebe on 2/05/25.
+//
+
+import Testing
+@testable import Snack_App
+
+struct Snack_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
